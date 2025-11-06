a71155be926fde022f3fe148b21ba0084c79ea4b
LFDdetailing <lfdcardetailing@gmail.com>
2025-11-05 23:27:37 -0300

    primera carga

---
+++ menu.js
@@ -0,0 +1,21 @@
+
+
+//menú desplegable
+
+const btnMenu = document.querySelector("#boton-menu");
+const btnCerrar = document.querySelector("#boton-cerrar");
+const navbar= document.querySelector(".menu-movil");
+
+btnMenu.addEventListener("click", deslizarMenu);
+btnCerrar.addEventListener("click", cerrarMenu);
+
+function deslizarMenu() {
+    
+    navbar.classList.toggle("active");
+            
+}
+
+function cerrarMenu(){
+
+    navbar.classList.toggle("active");
+}
